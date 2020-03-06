691f6b58e0c9b938719c072a39e6946f385b3699
sprored <josea.mc@gmail.com>
2020-03-06 12:23:53 +0100

    feat: Sohw categories using viewpager

---
+++ app/src/main/java/com/jamc68/miwok/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+
+package com.jamc68.miwok;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1){
+            return new FamilyFragment();
+        } else if (position == 2){
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
+
